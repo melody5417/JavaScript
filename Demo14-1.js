ab5b4a193e2f808b4f97bea6449f20d3e4f55037
yiqiwang <yiqiwang@tencent.com>
2021-03-28 18:12:19 +0800

    Add Demo14-1

---
+++ Demo14-1.js
@@ -0,0 +1,29 @@
+invoke(print, 500, 3000, 10000);
+
+function print() {
+	console.log('print');
+}
+
+function invoke(f, start = 0, interval, end) {
+	if (arguments.length <= 2) {
+		setTimeout(() => {
+			f();
+		}, start);
+		return;
+	}
+	setTimeout(() => {
+		repeat();
+	}, start);
+
+	function repeat() {
+		f();
+		const intervalPointer = setInterval(() => {
+			f();
+		}, interval);
+		if (end) {
+			setTimeout(() => {
+				clearInterval(intervalPointer);
+			}, end);
+		}
+	}
+}
