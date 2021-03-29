8b51d250db0a2ebde0af2e25af0adb997de4fdb6
TENCENT\yiqiwang <yiqiwang@tencent.com>
2021-03-29 19:36:55 +0800

    Update Demo14-1

--- Demo14-1.js
+++ Demo14-1.js
@@ -6,20 +6,14 @@ function print() {
 
 function invoke(f, start = 0, interval, end) {
 	if (arguments.length <= 2) {
-		setTimeout(() => {
-			f();
-		}, start);
+		setTimeout(f, start);
 		return;
 	}
-	setTimeout(() => {
-		repeat();
-	}, start);
+	setTimeout(repeat, start);
 
 	function repeat() {
 		f();
-		const intervalPointer = setInterval(() => {
-			f();
-		}, interval);
+		const intervalPointer = setInterval(f, interval);
 		if (end) {
 			setTimeout(() => {
 				clearInterval(intervalPointer);
